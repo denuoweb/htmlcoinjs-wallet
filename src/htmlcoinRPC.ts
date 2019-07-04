2f35e519cfed0fb2008d2865eeec578cafb3b32a
Jaron Rosenau <jaron.rosenau@gmail.com>
2019-07-04 01:02:00 -0700

    Update and rename qtumRPC.ts to htmlcoinRPC.ts

--- src/htmlcoinRPC.ts
+++ src/htmlcoinRPC.ts
@@ -1,8 +1,8 @@
-import RpcClient, {IConfig} from "qtumd-rpc"
+import RpcClient, {IConfig} from "htmlcoind-rpc"
 
 import {Network, NetworkNames} from "./Network"
 
-export default class QtumRPC {
+export default class HtmlcoinRPC {
   public rpc: RpcClient
 
   constructor(config?: IConfig) {
@@ -21,8 +21,8 @@ export default class QtumRPC {
   }
 }
 
-export const rpcClient = new QtumRPC({
-  user: "qtum",
+export const rpcClient = new HtmlcoinRPC({
+  user: "htmlcoin",
   pass: "test",
   port: "18332",
   protocol: "http",
